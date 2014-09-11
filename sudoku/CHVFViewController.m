c0c169134cdb109f6bb5d2f79ddcddf544ef5fb3
Cyrus Huang <cyrushuang@wl-200-242.wpa.claremont.edu>
2014-09-11 16:00:46 -0700

    first commit, single button added

--- sudoku/CHVFViewController.m
+++ sudoku/CHVFViewController.m
@@ -8,7 +8,10 @@
 
 #import "CHVFViewController.h"
 
-@interface CHVFViewController ()
+@interface CHVFViewController () {
+    UIButton* _button;
+    UIView* _gridView;
+}
 
 @end
 
@@ -17,13 +20,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // create grid view
+    CGRect frame = self.view.frame;
+    CGFloat x    = CGRectGetWidth(frame) * .1;
+    CGFloat y    = CGRectGetHeight(frame) * .1;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame)) * .8;
+    
+    CGRect gridFrame = CGRectMake(x, y, size, size);
+    
+    // create grid view
+    _gridView = [[UIView alloc] initWithFrame:gridFrame];
+    _gridView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_gridView];
+    
+    // create button
+    CGFloat buttonSize = size/5.0;
+    CGRect buttonFrame = CGRectMake(0, 0, buttonSize, buttonSize);
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button.tag = 1;
+    _button.backgroundColor = [UIColor redColor];
+    [_gridView addSubview:_button];
+    
+    // create target for button
+    [_button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed:(id)sender
+{
+    UIButton* button = (UIButton*) sender;
+    NSLog(@"Button %d was pressed", button.tag);
 }
 
 @end
