c3e209c7faed007bfb0a301e6282bd6f77d30871
Cyrus Huang <cyrushuang@wl-200-242.wpa.claremont.edu>
2014-09-14 19:14:43 -0700

    add comments and switch col/row

--- sudoku/CHVFGridView.m
+++ sudoku/CHVFGridView.m
@@ -19,24 +19,38 @@
 
 - (id) initWithFrame:(CGRect)frame {
     // create frame
-    CGFloat x    = CGRectGetWidth(frame) * .1;
-    CGFloat y    = CGRectGetHeight(frame) * .1;
-    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame)) * .8;
+    float framePortion = 0.8;
+    CGFloat x    = CGRectGetWidth(frame) * (1 - framePortion) / 2;
+    CGFloat y    = CGRectGetHeight(frame) * (1 - framePortion) / 2;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame)) * framePortion;
     CGRect gridFrame = CGRectMake(x, y, size, size);
     
     self = [super initWithFrame:gridFrame];
     self.backgroundColor = [UIColor blackColor];
     
-    // create buttons
-    CGFloat bigGap = size/40;
-    CGFloat smallGap = size/80;
-    CGFloat buttonSize = (size - (bigGap*4) - (smallGap*10)) / 9.0;
+    // calculate the size of the spacing between cells and blocks
+    float cellSeparatorPortion  = 1 / 80.0;
+    float blockSeparatorPortion = 1 / 40.0; // the additional width separations between blocks
+    CGFloat cellSeparatorWidth  = size * cellSeparatorPortion;
+    CGFloat blockSeparatorWidth = size * blockSeparatorPortion;
+    // the total grid size = 9 buttons + 4 block separators + 10 cell separators
+    // the button size is calculated by the equation above
+    CGFloat buttonSize = (size - (blockSeparatorWidth*4) - (cellSeparatorWidth*10)) / 9.0;
+    
+    // set up buttons (cells)
     _cells = [[NSMutableArray alloc] init];
     for (int row = 0; row < 9; row++) {
+        // create an array of nine buttons that makes up a row
         NSMutableArray* rowArray = [[NSMutableArray alloc] init];
         for (int col = 0; col < 9; col++) {
-            CGFloat x = ((col / 3) + 1) * bigGap + (col + 1) * smallGap + col * buttonSize;
-            CGFloat y = ((row / 3) + 1) * bigGap + (row + 1) * smallGap + row * buttonSize;
+            // calculate the number of separators to the left/top of the button
+            int blockSepLeftNum = (col / 3) + 1;
+            int blockSepTopNum  = (row / 3) + 1;
+            int cellSepLeftNum  = col + 1;
+            int cellSepTopNum   = row + 1;
+            // calculate the coordinate of the top left corner of the button
+            CGFloat x = blockSepLeftNum * blockSeparatorWidth + cellSepLeftNum * cellSeparatorWidth + col * buttonSize;
+            CGFloat y = blockSepTopNum * blockSeparatorWidth  + cellSepTopNum * cellSeparatorWidth  + row * buttonSize;
             CGRect buttonFrame = CGRectMake(x, y, buttonSize, buttonSize);
             UIButton* button = [[UIButton alloc] initWithFrame:buttonFrame];
             button.tag = row * 10 + col; // e.g: for the cell of row 2 col 7, the tag is 27
@@ -47,7 +61,6 @@
             [self addSubview:button];
             [rowArray addObject:button];
             
-            // create target for button
             [button addTarget:self action:@selector(cellSelected:) forControlEvents:UIControlEventTouchUpInside];
         }
         [_cells addObject:rowArray];
@@ -56,6 +69,7 @@
     return self;
 }
 
+// the number of 0 is used to represent blank
 - (void)setValueAtRow:(int)row col:(int)col to:(int)value {
     UIButton* button = _cells[row][col];
     if (value == 0) {
